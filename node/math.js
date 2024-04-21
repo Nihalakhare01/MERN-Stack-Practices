ffd39bc6a304cf8a4c18a49e713f27234cca4867
Nihal <thisisnihal95@gmail.com>
2024-04-22 00:19:13 +0530

    node

---
+++ node/math.js
@@ -0,0 +1,27 @@
+// const sum = (a,b) => a+b;
+module.exports.sum = (a,b) => a+b;
+
+module.exports.mul = (a,b) => a*b;
+
+
+module.exports.g = 9.8;
+module.exports.pi = 3.14;
+
+// let obj = {
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+//     pi:pi
+// };
+ 
+// module.exports  = 234;
+// module.exports = "hello world!";
+// module.exports = obj;
+
+// module.exports = {
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+//     pi:pi
+// };
+ 
---
+++ node/script.js
@@ -0,0 +1,21 @@
+// let n=5;
+
+// for(let i =0; i<5; i++){
+//     console.log(i);
+// }
+
+// console.log("bye!")
+
+// console.log(process.argv);
+
+// const val = require("./math");
+// console.log(val);
+// console.log(val.sum(2,2));
+// console.log(val.mul(2,3));
+// console.log(val.pi);
+
+// const val = require("./fruits/index");
+const val = require("./fruits");
+console.log(val);
+
+

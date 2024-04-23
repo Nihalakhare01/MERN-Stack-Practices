0ab763655b382faf709c5bf4b73a3668500b5be7
Nihal <thisisnihal95@gmail.com>
2024-04-23 21:54:31 +0530

    node

--- node/math.js
+++ node/math.js
@@ -1,11 +1,11 @@
 // const sum = (a,b) => a+b;
-module.exports.sum = (a,b) => a+b;
+// module.exports.sum = (a,b) => a+b;
 
-module.exports.mul = (a,b) => a*b;
+// module.exports.mul = (a,b) => a*b;
 
 
-module.exports.g = 9.8;
-module.exports.pi = 3.14;
+// module.exports.g = 9.8;
+// module.exports.pi = 3.14;
 
 // let obj = {
 //     sum:sum,
@@ -25,3 +25,7 @@ module.exports.pi = 3.14;
 //     pi:pi
 // };
  
+export const sum = (a,b) => a+b;
+export const mul = (a,b) => a*b;
+export const g = 9.8;
+export const pi = 3.14;
--- node/script.js
+++ node/script.js
@@ -1,5 +1,7 @@
 // let n=5;
 
+import { generate } from "random-words";
+
 // for(let i =0; i<5; i++){
 //     console.log(i);
 // }
@@ -19,13 +21,17 @@
 // console.log(val);
 
 
-const fig = require("figlet");
+// const fig = require("figlet");
 
-fig("hello world  !!", function(err,data){
-    if(err){
-        console.log("something went wrong..");
-        console.dir(err);
-        return;
-    }
-    console.log(data);
-});
+// fig("hello world  !!", function(err,data){
+//     if(err){
+//         console.log("something went wrong..");
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(data);
+// });
+
+// import {sum,pi} from "./math.js";
+// console.log(`The sum is ${sum(1+2)}`);
+

f399b3286e8687d0b3a8838f8cb6d7a753e4fb07
Nihal <thisisnihal95@gmail.com>
2024-05-19 13:50:32 +0530

    SQLclass

--- SQLclass/index.js
+++ SQLclass/index.js
@@ -2,7 +2,11 @@ const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 const express = require('express');
 const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended : true}));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 
@@ -23,34 +27,94 @@ let getRandomUser = () =>  {
     ];
   }
   
+
+  //home route
 app.get("/" , (req,res) => {
   let q = `SELECT count(*) FROM user`; 
   try{
      connection.query(q,(err, result) =>{
       if(err) throw err;
-        console.log(result[0]["count(*)"]);   
-        res.send("home.ejs");
+        let count = result[0]["count(*)"];   
+        res.render("home.ejs", {count});
       });  
     }catch (err){
          console.log(err);
          res.send("some error in DB");
     } 
-    //  res.send("welcome to home page");
 });
+
+
+// show Route
+app.get("/user" , (req,res) => {
+  let q = `SELECT * FROM user`; 
+  try{
+     connection.query(q,(err, users) =>{
+      if(err) throw err;
+        res.render("showuser.ejs" ,{users});
+      });  
+    }catch (err){
+         console.log(err);
+         res.send("some error in DB");
+    } 
+});
+
+
+// Edit Route
+app.get("/user/:id/edit" , (req,res) => {
+  let {id} = req.params;
+  let q = `SELECT * FROM user WHERE id = '${id}'`; 
+  try{
+     connection.query(q,(err, result) =>{
+      if(err) throw err;
+      let user = result[0];
+        res.render("edit.ejs", {user});
+      });  
+    }catch (err){
+         console.log(err);
+         res.send("some error in DB");
+    } 
+});
+
+
+//UPDATE ROUTE
+app.patch("/user/:id", (req,res) => {
+  let {id} = req.params;
+  let { password : formpass, username : newUsername } = req.body;
+  let q = `SELECT * FROM user WHERE id = '${id}'`; 
+  try{
+     connection.query(q,(err, result) =>{
+       if(err) throw err;
+        let user = result[0];
+        if(formpass != user.password){
+        res.send("wrong password");
+      }else{
+        let q2 = `UPDATE user SET username='${newUsername}' WHERE id='${id}'`;
+          connection.query(q2 , (err,result) => {
+          if(err) throw err; 
+          res.send(result);
+
+        })
+      }
+      res.send(user);
+        // res.render("edit.ejs", {user});
+      });  
+    }catch (err){
+         console.log(err);
+         res.send("some error in DB");
+    } 
+});
+
+
 
 app.listen(8080 , () => {
   console.log("Server is running on port 8080");
 });
-
-
-
 
  // data inserting in bulk
 // let data = [];
 // for(i=0; i<50 ; i++){
 //     data.push(getRandomUser());
 // }
-
 
 //   let q =  "INSERT INTO user (id, username, email, password) values ?";
 //   //    let user = [

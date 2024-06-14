4497a49d51350567fe18e102704549d50f5208d7
Nihal <thisisnihal95@gmail.com>
2024-06-15 00:48:45 +0530

    Mongo

--- Mongo/index.js
+++ Mongo/index.js
@@ -10,3 +10,62 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+const userschema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+});
+
+const User = mongoose.model("User", userschema);
+
+// User.findOneAndUpdate({name: "tony"}, {age:18},{new: true})
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+
+    User.deleteOne({name:"bruce"})
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+    // User.find({})
+    // .then((res) => {
+    //     console.log(res);
+    // })
+    // .catch((err) => {
+    //     console.log(err);
+    // });
+
+// User.insertMany([
+//     {name:"tony", email:"tony@gmail.com",age:50},
+//     {name:"bruce", email:"bruce@gmail.com",age:40},
+// ]).then((res)=>{
+//     console.log(res)
+// })
+//     .catch((err)=>{
+//         console.log(err);
+//     });
+
+// const user2 = new User({
+//     name: "eve",
+//     email:"asdf@gmail.com",
+//     age:49,
+// });
+
+// user2
+//     .save()
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });

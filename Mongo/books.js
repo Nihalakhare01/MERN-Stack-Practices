5d87111af8bdbbfed8de3a26d383689bf4a2490b
Nihal <thisisnihal95@gmail.com>
2024-06-15 19:34:05 +0530

    Mongo2

--- Mongo/books.js
+++ Mongo/books.js
@@ -6,7 +6,7 @@ main()
     })
     .catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+  await mongoose.connect("mongodb://127.0.0.1:27017/amazon");
 }
 
 
@@ -14,13 +14,44 @@ const bookschema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
+        maxlength: 20,
     },
     author: {
         type: String,
     },
     price: {
-        type:Number,   
+        type:Number, 
+        min: [1, "price is too low "],  
     },
+    discount:{
+        type:Number,
+        default:0,
+    },
+    genre: [String]
 });
 
 const Book = mongoose.model("Book", bookschema);
+
+// let book1 = new Book({
+//     title:"Science",
+//     author:"RD Sharma",
+//     price:1299
+// });
+
+// book1
+// .save()
+// .then((res) => {
+//     console.log(res);
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
+
+
+Book.findByIdAndUpdate('666d749792b21f3db882aa9d', {price:2220}, {runValidators:true})
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
